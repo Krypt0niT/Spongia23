95587e9308ede81ac2e6b282e1728c58d23b584b
NikolasMichalicka <michalickanikolas@gmail.com>
2023-10-28 00:14:40 +0200

    co to
    kys

--- Spongia23/Assets/Scripts/ChangeTexture.cs
+++ Spongia23/Assets/Scripts/ChangeTexture.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 public class ChangeTexture : MonoBehaviour
 {
     public Sprite Sprite;
+    public ParticleSystem Particle;
+    public float time;
 
     public void Change()
+    {
+        if (Particle != null) Particle.Play();
+        Invoke("ChangeTextur", time);
+    }
+
+    void ChangeTextur()
     {
         GetComponent<SpriteRenderer>().sprite = Sprite;
     }
