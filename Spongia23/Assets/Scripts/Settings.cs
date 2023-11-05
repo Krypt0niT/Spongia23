2d248c9b4cc3754aa287ea1191d78f5a10afee7a
Krypt0niT <k269692@gmail.com>
2023-11-05 21:10:45 +0100

    Bowser new theme

---
+++ Spongia23/Assets/Scripts/Settings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public float MusicVolume;
+    public float SoundEffectsVolume;
+
+    public bool MuteMusic;
+    public bool MuteSoundEffects;
+
+}
